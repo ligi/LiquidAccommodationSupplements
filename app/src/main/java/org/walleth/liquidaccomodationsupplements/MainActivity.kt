68b992575d90dcbb25d6054d4aa91f1623645d6d
ligi <ligi@ligi.de>
2018-10-25 09:55:18 +0200

    Option items for info and debug

--- app/src/main/java/org/walleth/liquidaccomodationsupplements/MainActivity.kt
+++ app/src/main/java/org/walleth/liquidaccomodationsupplements/MainActivity.kt
@@ -8,21 +8,23 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.supplement_item.view.*
+import org.ligi.kaxt.startActivityFromURL
 import org.ligi.kaxtui.alert
 import org.walleth.liquidaccomodationsupplements.model.AccommodationSupplement
 import org.walleth.liquidaccomodationsupplements.model.AccommodationSupplements
+import org.walleth.liquidaccomodationsupplements.model.liquiditynetwork.Invoice
 import org.walleth.liquidaccomodationsupplements.model.liquiditynetwork.InvoiceRequest
 import org.walleth.liquidaccomodationsupplements.model.liquiditynetwork.WalletInformation
 
 var currentSelectedSupplement = AccommodationSupplements.first()
 val adapter = ItemAdapter(AccommodationSupplements)
+
 var walletInformation: WalletInformation? = null
+var currentInvoice: Invoice? = null
 
 val api by lazy {
     LiquidityAPI("http://10.0.2.2:3600")
@@ -35,7 +37,7 @@ class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         itemView.supplement_price.text = supplement.amount.toString() + " " + supplement.currency
 
         if (supplement == currentSelectedSupplement) {
-            itemView.supplement_card.setBackgroundColor(Color.argb(0xff,0,0xcc,0))
+            itemView.supplement_card.setBackgroundColor(Color.argb(0xff, 0, 0xcc, 0))
         } else {
             itemView.supplement_card.setBackgroundColor(Color.WHITE)
         }
@@ -69,7 +71,6 @@ class ItemAdapter(private val supplements: List<AccommodationSupplement>) : Recy
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -79,28 +80,39 @@ class MainActivity : AppCompatActivity() {
 
         recycler_view.adapter = adapter
 
-                Thread(Runnable {
+        Thread(Runnable {
 
-                    walletInformation = api.getWalletInformation()
+            walletInformation = api.getWalletInformation()
 
-                    if (walletInformation == null) {
-                        runOnUiThread {
-                            alert("Could not load wallet information")
-                        }
-                    } else {
-                        createInvoice()
+            if (walletInformation == null) {
+                runOnUiThread {
+                    alert("Could not load wallet information")
+                }
+            } else {
+                createInvoice()
 
-                        while (true) {
-                            SystemClock.sleep(100)
-                            val transactions = api.getTransactions()
-                            Log.i("", "transactions:" + transactions.toString())
-                        }
-                    }
-                }).start()
+                while (true) {
+                    SystemClock.sleep(100)
+                    val transactions = api.getTransactions()
+                    Log.i("", "transactions:" + transactions.toString())
+                }
+            }
+        }).start()
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_info -> startActivityFromURL("https://github.com/ligi/LiquidAccommodationSupplements")
+            R.id.menu_debug -> alert("address" + walletInformation?.address + "\ninvoice nonce: " + currentInvoice?.nonce)
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
 
 private fun Activity.createInvoice() {
@@ -109,10 +121,10 @@ private fun Activity.createInvoice() {
         val address = walletInformation!!.address
 
         val invoiceRequest = InvoiceRequest(currentSelectedSupplement.amount, address)
-        val invoice = api.requestInvoice(invoiceRequest)
+        currentInvoice = api.requestInvoice(invoiceRequest)
 
         runOnUiThread {
-            qr_code.setQRCode(invoice?.encoded?.raw!!)
+            qr_code.setQRCode(currentInvoice?.encoded?.raw!!)
         }
     }).start()
 }
